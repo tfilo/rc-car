377f7ee94a6bf4ee3ceb330454c8789cdedb02b0
Tomáš Filo <tfilosk@gmail.com>
2026-01-02 11:38:23 +0100

    Further refactor

--- src/main.py
+++ src/main.py
@@ -1,14 +1,7 @@
-from ure import search
 from time import sleep_ms
 from rc_car import RcCar
 from battery import Battery
-from server import (
-    Server,
-    STATIC_INDEX_RESPONSE,
-    STATIC_STYLE_RESPONSE,
-    STATIC_CONTROL_RESPONSE,
-    STATIC_NOT_FOUND_RESPONSE,
-)
+from server import Server
 
 # import os
 
@@ -38,50 +31,19 @@ while True:
     voltage = battery.read_voltage()
     try:
         counter += 1
-        client, address = server.accept_client()
-        request = client.recv(2048).decode()
 
         if counter > 1000:
             voltage = battery.read_voltage()
             counter = 0
-        
-        response = None
-        if request.startswith("POST /control"):
-            match = search(
-                r"steering=([-0-9]+)&drive=([-0-9]+)&horn=([-0-9]+)&light=([-0-9]+)",
-                request,
-            )
-            if match:
-                rc_car.update(
-                    int(match.group(1)),
-                    int(match.group(2)),
-                    bool(int(match.group(3))),
-                    bool(int(match.group(4))),
-                )
 
-            response = server.success_response(voltage)
+        request = server.accept_client(voltage)
 
-        elif request.startswith("GET / "):
-            response = STATIC_INDEX_RESPONSE
+        if request is not None:
+            rc_car.update(request[0], request[1], request[2], request[3])
 
-        elif request.startswith("GET /style.css "):
-            response = STATIC_STYLE_RESPONSE
-
-        elif request.startswith("GET /control.js "):
-            response = STATIC_CONTROL_RESPONSE
-
-        else:
-            response = STATIC_NOT_FOUND_RESPONSE
-
-        client.send(response)
     except KeyboardInterrupt:
         raise
     except Exception as e:
         print("WARN: wifi/server thread restart:", e)
-    finally:
-        try:
-            if client:
-                client.close()
-        except Exception as e:
-            print("WARN: wifi/server client.close() error:", e)
+
     sleep_ms(10)
--- src/server.py
+++ src/server.py
@@ -1,3 +1,4 @@
+from ure import search
 from time import sleep_ms
 import network
 import rp2
@@ -20,15 +21,20 @@ index_html = load_file("index.html")
 style_css = load_file("style.css")
 control_js = load_file("control.js")
 
-STATIC_INDEX_RESPONSE = "HTTP/1.1 200 OK\r\n" "Content-Type: text/html\r\n\r\n" + index_html
+STATIC_INDEX_RESPONSE = (
+    "HTTP/1.1 200 OK\r\n" "Content-Type: text/html\r\n\r\n" + index_html
+)
 
-STATIC_STYLE_RESPONSE = "HTTP/1.1 200 OK\r\n" "Content-Type: text/css\r\n\r\n" + style_css
+STATIC_STYLE_RESPONSE = (
+    "HTTP/1.1 200 OK\r\n" "Content-Type: text/css\r\n\r\n" + style_css
+)
 
 STATIC_CONTROL_RESPONSE = (
     "HTTP/1.1 200 OK\r\n" "Content-Type: application/javascript\r\n\r\n" + control_js
 )
 
 STATIC_NOT_FOUND_RESPONSE = "HTTP/1.1 404 Not Found\r\n\r\n"
+
 
 class Server:
 
@@ -54,15 +60,50 @@ class Server:
         self.s.listen(1)
         print("HTTP server running")
 
-    def accept_client(self):
-        client, address = self.s.accept()
-        return client, address
+    def accept_client(self, voltage):
+        client = self.s.accept()[0]
 
-    def success_response(self, voltage):
+        request = client.recv(2048).decode()
+        result = None
+
+        if request.startswith("POST /control"):
+            response = self.__success_response(voltage)
+            match = search(
+                r"steering=([-0-9]+)&drive=([-0-9]+)&horn=([-0-9]+)&light=([-0-9]+)",
+                request,
+            )
+            if match:
+                result = (
+                    int(match.group(1)),
+                    int(match.group(2)),
+                    bool(int(match.group(3))),
+                    bool(int(match.group(4))),
+                )
+
+        elif request.startswith("GET / "):
+            response = STATIC_INDEX_RESPONSE
+
+        elif request.startswith("GET /style.css "):
+            response = STATIC_STYLE_RESPONSE
+
+        elif request.startswith("GET /control.js "):
+            response = STATIC_CONTROL_RESPONSE
+
+        else:
+            response = STATIC_NOT_FOUND_RESPONSE
+
+        client.send(response)
+        try:
+            client.close()
+        except Exception as e:
+            print("WARN: wifi/server client.close() error:", e)
+
+        return result
+
+    def __success_response(self, voltage):
         response = (
             "HTTP/1.1 200 OK\r\n"
             "Content-Type: application/json\r\n\r\n"
             '{"status":"ok", "battery":"' + str(voltage) + '"}'
         )
         return response
-
